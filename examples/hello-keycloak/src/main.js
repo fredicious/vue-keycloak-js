7b049aa48854b425ca6659da9a5f2cb741fc3a6b
david0718 <david0718@qq.com>
2020-03-16 16:41:09 +0800

    update

--- examples/hello-keycloak/src/main.js
+++ examples/hello-keycloak/src/main.js
@@ -1,9 +1,16 @@
 import VueKeycloakJs from '@gemtmp/vue-keycloak-js'
+import KeycloakAuthorization from 'keycloak-js/dist/keycloak-authz'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Vue.test = (function name(params) {
+//   VueKeycloakJs.keycloak.authServerUrl = "http://124.93.26.52:63156/auth/";
+//   return new KeycloakAuthorization(VueKeycloakJs.keycloak);
+//  }
+// )(),
 
 Vue.use(VueKeycloakJs, {
   init: {
@@ -12,15 +19,20 @@ Vue.use(VueKeycloakJs, {
     onLoad: 'login-required'
   },
   config: {
-    url: 'http://124.93.26.52:63156/auth/',
+    url: 'http://124.93.26.52:63156/auth',
     clientId: 'hello-world',
     realm: 'poc'
   },
   onReady: (keycloak) => {
+    keycloak.loadUserProfile().success((data) => {
+      console.log(data);
+      const client = new KeycloakAuthorization(keycloak);
+      console.log(client);
+    }),
     new Vue({
       router,
       render: h => h(App)
-    }).$mount('#app')
+    }).$mount('#app');
   }
-})
+});
 
--- examples/hello-keycloak/src/views/Secret.vue
+++ examples/hello-keycloak/src/views/Secret.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>This is a secret about page. You should only be able to see this if you are authenticated.</h1>
     <h3>This is what my token looks like:</h3>
-    <code>{{ $keycloak.tokenParsed }}</code>
+    <label>tokenParsed</label><code>{{ $keycloak.tokenParsed }}</code>
+    <br/>
+    <label>idTokenParsed</label><code>{{ $keycloak.idTokenParsed }}</code>
+    <br/>
+    <!-- <label>Authorization</label><code>{{ $test }}</code> -->
+    <br/>
   </div>
 </template>
